3b0c3c4c4aace05dc1dd123f5a545217e9be2e9b
MAYCOLRICHARDCLAUDIOPINEDO18 <1353921@senati.pe>
2022-11-05 18:29:53 -0500

    mejorado

--- resources/views/lima.blade.php
+++ resources/views/lima.blade.php
@@ -20,27 +20,28 @@
       </div>
       <div class="carousel-inner">
         <div class="carousel-item" data-bs-interval="10000">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Primera diapositiva" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="1298388"><title _mstHash="1093950" _mstTextHash="177515" _mstHidden="1">Placeholder</title><rect width="100%" height="100%" fill="#f5f5f5"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em" _mstHash="1483417" _mstTextHash="386737">Primera diapositiva</text></svg>
+         <img src="https://www.bing.com/th?id=OSK.HERO0ziWssR98sg8Xm65QPsYzSsANhpWxDBObVm0FKQ33e8&w=472&h=280&c=1&rs=2&o=6&pid=SANGAM" alt="">
     
           <div class="carousel-caption d-none d-md-block">
-            <h5 _msthash="1293084" _msttexthash="795847">Primera etiqueta de diapositiva</h5>
-            <p _msthash="1265459" _msttexthash="3833947">Contenido de marcador de posición representativo para la primera diapositiva.</p>
+            <h5 _msthash="1293084" _msttexthash="795847">CUZCO</h5>
+            <p _msthash="1265459" _msttexthash="3833947">HOLA</p>
           </div>
         </div>
         <div class="carousel-item" data-bs-interval="2000" _mstvisible="0">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Segunda diapositiva" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="1294995" _mstVisible="1"><title _mstHash="1094431" _mstTextHash="177515" _mstVisible="2">Placeholder</title><rect width="100%" height="100%" fill="#eee" _mstVisible="2"></rect><text x="50%" y="50%" fill="#bbb" dy=".3em" _mstHash="1483898" _mstTextHash="178087" _mstVisible="2">Second slide</text></svg>
+         <img src="https://www.bing.com/th?u=https%3a%2f%2ftse2.mm.bing.net%2fth%3fid%3dOIP.3I9DGAdXOcSIL-dn77PK1QHaE8&ehk=wYY2vgmy4yUNZpu0qRsE5hp9CWeEjKrEeDVhCFFXtqo%3d&w=130&h=100&c=8&o=6&pid=AppEx" alt="">
+    
     
           <div class="carousel-caption d-none d-md-block" _mstvisible="1">
-            <h5 _msthash="1293565" _msttexthash="794677" _mstvisible="2">Segunda etiqueta de diapositiva</h5>
-            <p _msthash="1265940" _msttexthash="3827044" _mstvisible="2">Contenido de marcador de posición representativo para la segunda diapositiva.</p>
+            <h5 _msthash="1293565" _msttexthash="794677" _mstvisible="2">LIMA</h5>
+            <p _msthash="1265940" _msttexthash="3827044" _mstvisible="2">HOLA</p>
           </div>
         </div>
         <div class="carousel-item active" _mstvisible="0">
-          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Tercera diapositiva" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="1294683" _mstVisible="1"><title _mstHash="1094912" _mstTextHash="177515" _mstVisible="2">Placeholder</title><rect width="100%" height="100%" fill="#e5e5e5" _mstVisible="2"></rect><text x="50%" y="50%" fill="#999" dy=".3em" _mstHash="1484379" _mstTextHash="155675" _mstVisible="2">Third slide</text></svg>
+          <img src="https://th.bing.com/th/id/OIP.paLpVagW7Wl8NOT1kqd5VAHaD4?w=340&h=180&c=7&r=0&o=5&pid=1.7" alt="">
     
           <div class="carousel-caption d-none d-md-block" _mstvisible="1">
-            <h5 _msthash="1294046" _msttexthash="794612" _mstvisible="2">Tercera etiqueta de diapositiva</h5>
-            <p _msthash="1266421" _msttexthash="3826342" _mstvisible="2">Contenido de marcador de posición representativo para la tercera diapositiva.</p>
+            <h5 _msthash="1294046" _msttexthash="794612" _mstvisible="2">ANCASH</h5>
+            <p _msthash="1266421" _msttexthash="3826342" _mstvisible="2">HOLA</p>
           </div>
         </div>
       </div>
