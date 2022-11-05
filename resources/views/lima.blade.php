f583c5435c2995dde73b92b376cb96454d526a64
MAYCOLRICHARDCLAUDIOPINEDO18 <1353921@senati.pe>
2022-11-05 18:18:26 -0500

    img

---
+++ resources/views/lima.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-3 m-0 border-0 bd-example">
+
+    <!-- Example Code -->
+    
+    <div id="carouselExampleDark" class="carousel carousel-dark slide pointer-event" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="" aria-label="Diapositiva 1" _mstaria-label="193609"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Diapositiva 2" _mstaria-label="193843" class=""></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Diapositiva 3" _mstaria-label="194077" class="active" aria-current="true"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item" data-bs-interval="10000">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Primera diapositiva" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="1298388"><title _mstHash="1093950" _mstTextHash="177515" _mstHidden="1">Placeholder</title><rect width="100%" height="100%" fill="#f5f5f5"></rect><text x="50%" y="50%" fill="#aaa" dy=".3em" _mstHash="1483417" _mstTextHash="386737">Primera diapositiva</text></svg>
+    
+          <div class="carousel-caption d-none d-md-block">
+            <h5 _msthash="1293084" _msttexthash="795847">Primera etiqueta de diapositiva</h5>
+            <p _msthash="1265459" _msttexthash="3833947">Contenido de marcador de posición representativo para la primera diapositiva.</p>
+          </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000" _mstvisible="0">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Segunda diapositiva" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="1294995" _mstVisible="1"><title _mstHash="1094431" _mstTextHash="177515" _mstVisible="2">Placeholder</title><rect width="100%" height="100%" fill="#eee" _mstVisible="2"></rect><text x="50%" y="50%" fill="#bbb" dy=".3em" _mstHash="1483898" _mstTextHash="178087" _mstVisible="2">Second slide</text></svg>
+    
+          <div class="carousel-caption d-none d-md-block" _mstvisible="1">
+            <h5 _msthash="1293565" _msttexthash="794677" _mstvisible="2">Segunda etiqueta de diapositiva</h5>
+            <p _msthash="1265940" _msttexthash="3827044" _mstvisible="2">Contenido de marcador de posición representativo para la segunda diapositiva.</p>
+          </div>
+        </div>
+        <div class="carousel-item active" _mstvisible="0">
+          <svg class="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Marcador de posición: Tercera diapositiva" preserveAspectRatio="xMidYMid slice" focusable="false" _mstaria-label="1294683" _mstVisible="1"><title _mstHash="1094912" _mstTextHash="177515" _mstVisible="2">Placeholder</title><rect width="100%" height="100%" fill="#e5e5e5" _mstVisible="2"></rect><text x="50%" y="50%" fill="#999" dy=".3em" _mstHash="1484379" _mstTextHash="155675" _mstVisible="2">Third slide</text></svg>
+    
+          <div class="carousel-caption d-none d-md-block" _mstvisible="1">
+            <h5 _msthash="1294046" _msttexthash="794612" _mstvisible="2">Tercera etiqueta de diapositiva</h5>
+            <p _msthash="1266421" _msttexthash="3826342" _mstvisible="2">Contenido de marcador de posición representativo para la tercera diapositiva.</p>
+          </div>
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden" _msthash="1040793" _msttexthash="116246">Anterior</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden" _msthash="1041235" _msttexthash="113945">Próximo</span>
+      </button>
+    </div>
+    
+    <!-- End Example Code -->
+  </body>
+</html>
